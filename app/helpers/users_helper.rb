e8b2de5c5ef5956e296b06dbefb56f8b1d4e6f67
Tanaka <sample@email.com>
2020-02-18 15:25:17 +0000

    Finish add-basic-info

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,9 @@ module UsersHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+	
+	# 勤怠基本情報を指定のフォーマットで返します。
+	def format_basic_info(time)
+		format("%.2f",((time.hour * 60) + time.min) / 60.0)
+	end
 end
